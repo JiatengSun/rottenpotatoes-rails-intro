09c4af4876cde4b6feb3e6b8049790b574df0a46
EC2 Default User <ec2-user@ip-172-31-23-10.us-east-2.compute.internal>
2021-02-26 02:28:52 +0000

    part 1 1

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -4,11 +4,19 @@ class Movie < ActiveRecord::Base
       return ['G', 'PG', 'PG-13' ,'R']
     end
     
-    def self.with_ratings(ratings)
-        if ratings.nil?
-            Movie.all
+    def self.with_ratings(ratings_list, sort_by)
+        if !ratings_list.empty?
+            if sort_by.empty?
+                return self.where(rating: ratings_list)
+            else
+                return self.where(rating: ratings_list).order(sort_by)
+            end
         else
-            Movie.where(rating:ratings)
+            if sort_by.empty?
+                return self.all
+            else
+                return self.all.order(sort_by)
+            end
         end
     end
 end
